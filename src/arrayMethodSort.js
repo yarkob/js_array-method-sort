e6ee3fe52ecca1b85c6bcb4ed2cff9ac9361d9f7
Yaroslav <yaroslav.klark.bondarenko@gmail.com>
2023-10-19 14:41:44 +0300

    Initial commit

---
+++ src/arrayMethodSort.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method Sort
+ */
+function applyCustomSort() {
+  [].__proto__.sort2 = function(compareFunction) {
+    // write code here
+  };
+}
+
+module.exports = applyCustomSort;
